189d3dd19839501a55c9a7cf80a74f8574f6b49c
nevitsho-hash <nevits.ho@gmail.com>
2026-03-01 14:57:23 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,50 @@
+const sonidoBoton = new Audio('assets/sng/clic.mp3');
+let pokemonDetectado = true; // Empieza en true porque Gengar ya está en pantalla
+let html5QrCode;
+
+const pokemonDB = {
+    "GENGAR": { 
+        text: "¡HAS ENCONTRADO<br>A GENGAR!<br>LA SOMBRA TRAVIESA", 
+        sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/94.png",
+        cry: "assets/sng/gengar.mp3"
+    }
+};
+
+function activarEscaner() {
+    sonidoBoton.play().catch(() => {});
+    document.querySelector('.pokedex').classList.add('scanning');
+    document.getElementById('pokedex-content').style.display = 'none';
+    document.getElementById('reader').style.display = 'block';
+    
+    if (!html5QrCode) { html5QrCode = new Html5Qrcode("reader"); }
+    
+    html5QrCode.start({ facingMode: "environment" }, { fps: 15, qrbox: { width: 180, height: 180 } }, (decodedText) => {
+        let code = decodedText.toUpperCase().trim();
+        if (pokemonDB[code]) {
+            const data = pokemonDB[code];
+            html5QrCode.stop().then(() => { actualizarPantalla(data); });
+        }
+    }).catch((err) => console.error(err));
+}
+
+function actualizarPantalla(data) {
+    document.querySelector('.pokedex').classList.remove('scanning');
+    document.getElementById('reader').style.display = 'none';
+    document.getElementById('pokedex-content').style.display = 'flex';
+    document.getElementById('main-text').innerHTML = data.text;
+    document.getElementById('main-sprite').src = data.sprite;
+    document.getElementById('main-sprite').classList.remove('shaking-ball');
+    pokemonDetectado = true;
+    setTimeout(() => { new Audio(data.cry).play().catch(() => {}); }, 300); 
+}
+
 function capturarPokemon() {
     if (!pokemonDetectado) return;
     const sprite = document.getElementById('main-sprite');
     const texto = document.getElementById('main-text');
-
-    // 1. Cambiamos el Pokémon por tu Pokéball
-    sprite.src = 'assets/img/pokeball.png'; 
+    sprite.src = 'assets/img/pokeball.png'; // Tu imagen pixelada
     sprite.classList.add('shaking-ball'); 
     texto.innerHTML = "¡ATRÁPALO!";
-
-    // 2. Animación de 3 segundos
     setTimeout(() => {
         sprite.classList.remove('shaking-ball');
         texto.innerHTML = "¡POKÉMON ATRAPADO!<br>REGISTRADO CON ÉXITO";
