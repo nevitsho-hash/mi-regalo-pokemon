c4b16719c9cf8c1bd3607beaa302f4cc3d6b0b19
nevitsho-hash <nevits.ho@gmail.com>
2026-03-01 17:36:50 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const sonidoBoton = new Audio('assets/sng/clic.mp3');
 let html5QrCode;
 let pokemonDetectado = true;
+
 const pokemonDB = {
 "BEAUTIFLY": { text: "¡MIRA ESA BEAUTIFLY!<br>SUS ALAS SON BELLAS", sprite: "assets/img/BEAUTIFLY.png", cry: "assets/sng/beautifly.mp3" },
 "SNORLAX": { text: "¡HAS ENCONTRADO A SNORLAX!", sprite: "assets/img/SNORLAX.png", cry: "assets/sng/snorlax.mp3" },
@@ -10,37 +11,42 @@ const pokemonDB = {
 "JIGGLYPUFF": { text: "¡HAS ENCONTRADO A JIGGLYPUFF!", sprite: "assets/img/JIGGLYPUFF.png", cry: "assets/sng/jigglypuff.mp3" },
 "GENGAR": { text: "¡HAS ENCONTRADO A GENGAR!<br>LA SOMBRA TRAVIESA", sprite: "assets/img/GENGAR.png", cry: "assets/sng/gengar.mp3" }
 };
+
 function activarEscaner() {
-sonidoBoton.play().catch(() => {});
-document.getElementById('pokedex-content').style.display = 'none';
-document.getElementById('reader').style.display = 'block';
-if (!html5QrCode) { html5QrCode = new Html5Qrcode("reader"); }
-html5QrCode.start({ facingMode: "environment" }, { fps: 15, qrbox: { width: 250, height: 200 } }, (text) => {
-let code = text.toUpperCase().trim();
-if (pokemonDB[code]) {
-html5QrCode.stop().then(() => { actualizarPantalla(pokemonDB[code]); });
+    sonidoBoton.play().catch(() => {});
+    document.getElementById('pokedex-content').style.display = 'none';
+    document.getElementById('reader').style.display = 'block';
+    if (!html5QrCode) { html5QrCode = new Html5Qrcode("reader"); }
+    html5QrCode.start({ facingMode: "environment" }, { fps: 15, qrbox: { width: 250, height: 200 } }, (text) => {
+        let code = text.toUpperCase().trim();
+        if (pokemonDB[code]) {
+            html5QrCode.stop().then(() => { actualizarPantalla(pokemonDB[code]); });
+        }
+    }).catch(err => console.error(err));
 }
-}).catch(err => console.error(err));
-}
+
 function actualizarPantalla(data) {
-document.getElementById('reader').style.display = 'none';
-document.getElementById('pokedex-content').style.display = 'flex';
-document.getElementById('main-text').innerHTML = data.text;
-document.getElementById('main-sprite').src = data.sprite;
-pokemonDetectado = true;
-setTimeout(() => { new Audio(data.cry).play().catch(() => {}); }, 300);
+    document.getElementById('reader').style.display = 'none';
+    document.getElementById('pokedex-content').style.display = 'flex';
+    document.getElementById('main-text').innerHTML = data.text;
+    document.getElementById('main-sprite').src = data.sprite;
+    document.getElementById('main-sprite').classList.remove('shaking-ball');
+    pokemonDetectado = true;
+    setTimeout(() => { new Audio(data.cry).play().catch(() => {}); }, 300);
 }
+
 function capturarPokemon() {
-if (!pokemonDetectado) return;
-const sprite = document.getElementById('main-sprite');
+    if (!pokemonDetectado) return;
+    const sprite = document.getElementById('main-sprite');
     
-// CAMBIO: Ahora carga una Pokéball roja
-sprite.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png';
-sprite.classList.add('shaking-ball');
-document.getElementById('main-text').innerHTML = "¡ATRÁPALO!";
-setTimeout(() => {
-sprite.classList.remove('shaking-ball');
-document.getElementById('main-text').innerHTML = "¡POKÉMON ATRAPADO!";
-pokemonDetectado = false;
-}, 3000);
+    // POKÉBALL ROJA PIXELADA (Sprite de batalla) [cite: 2026-03-01]
+    sprite.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/animated/poke-ball.gif';
+    sprite.classList.add('shaking-ball');
+    
+    document.getElementById('main-text').innerHTML = "¡ATRÁPALO!";
+    setTimeout(() => {
+        sprite.classList.remove('shaking-ball');
+        document.getElementById('main-text').innerHTML = "¡POKÉMON ATRAPADO!";
+        pokemonDetectado = false;
+    }, 3000);
 }
