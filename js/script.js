eb78085ab4e40316d95fc7318a323247fa265bcf
nevitsho-hash <nevits.ho@gmail.com>
2026-03-01 15:03:06 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,47 +1,87 @@
+scrip
+
 const sonidoBoton = new Audio('assets/sng/clic.mp3');
-let pokemonDetectado = true;
-let html5QrCode;
 
 const pokemonDB = {
-    "GENGAR": { text: "¡HAS ENCONTRADO A GENGAR!", sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/94.png", cry: "assets/sng/gengar.mp3" },
-    "UMBREON": { text: "¡HAS ENCONTRADO A UMBREON!", sprite: "assets/img/UMBREON.png", cry: "assets/sng/umbreon.mp3" }
-    // Añade aquí el resto de tus Pokémon...
+    "BEAUTIFLY": { 
+        text: "¡MIRA ESA BEAUTIFLY!<br>SUS ALAS SON BELLAS,<br>¡PERO TU ERES MAS<br>QUE CUALQUIER POKEMON!", 
+        sprite: "assets/img/BEAUTIFLY.png",
+        cry: "assets/sng/beautifly.mp3" 
+    },
+    "SNORLAX": { 
+        text: "¡HAS ENCONTRADO<br>A SNORLAX!<br>BLOQUEA EL CAMINO,<br>PERO NO A MI CORAZON", 
+        sprite: "assets/img/SNORLAX.png",
+        cry: "assets/sng/snorlax.mp3"
+    },
+    "SWALOT": { 
+        text: "¡HAS ENCONTRADO<br>A SWALOT!<br>EL POKEMON BOLSA", 
+        sprite: "assets/img/SWALOT.png",
+        cry: "assets/sng/swalot.mp3"
+    },
+    "TOTODILE": { 
+        text: "¡HAS ENCONTRADO<br>A TOTODILE!<br>EL COCODRILO ALEGRE", 
+        sprite: "assets/img/TOTODILE.png",
+        cry: "assets/sng/totodile.mp3"
+    },
+    "UMBREON": { 
+        text: "¡HAS ENCONTRADO<br>A UMBREON!<br>LUZ EN LA OSCURIDAD", 
+        sprite: "assets/img/UMBREON.png",
+        cry: "assets/sng/umbreon.mp3"
+    },
+    "JIGGLYPUFF": { 
+        text: "¡HAS ENCONTRADO<br>A JIGGLYPUFF!<br>CUIDADO CON SU CANTO", 
+        sprite: "assets/img/JIGGLYPUFF.png",
+        cry: "assets/sng/jigglypuff.mp3"
+    },
+    "GENGAR": { 
+        text: "¡HAS ENCONTRADO<br>A GENGAR!<br>LA SOMBRA TRAVIESA", 
+        sprite: "assets/img/GENGAR.png",
+        cry: "assets/sng/gengar.mp3"
+    }
 };
+
+let html5QrCode;
 
 function activarEscaner() {
     sonidoBoton.play().catch(() => {});
+    
+    // Activa la animación de las luces
+    document.querySelector('.pokedex').classList.add('scanning');
+    
     document.getElementById('pokedex-content').style.display = 'none';
-    document.getElementById('reader').style.display = 'block';
-    if (!html5QrCode) { html5QrCode = new Html5Qrcode("reader"); }
-    html5QrCode.start({ facingMode: "environment" }, { fps: 15, qrbox: 200 }, (decodedText) => {
-        let code = decodedText.toUpperCase().trim();
-        if (pokemonDB[code]) {
-            const data = pokemonDB[code];
-            html5QrCode.stop().then(() => { 
-                actualizarPantalla(data);
-            });
+    const readerElement = document.getElementById('reader');
+    readerElement.style.display = 'block';
+
+    if (!html5QrCode) {
+        html5QrCode = new Html5Qrcode("reader");
+    }
+
+    html5QrCode.start(
+        { facingMode: "environment" }, 
+        { fps: 15, qrbox: { width: 200, height: 200 } },
+        (decodedText) => {
+            let code = decodedText.toUpperCase().trim();
+            if (pokemonDB[code]) {
+                const data = pokemonDB[code];
+                html5QrCode.stop().then(() => {
+                    actualizarPantalla(data);
+                });
+            }
         }
-    }).catch((err) => console.error(err));
+    ).catch((err) => console.error(err));
 }
 
 function actualizarPantalla(data) {
+    // Detiene la animación de las luces
+    document.querySelector('.pokedex').classList.remove('scanning');
+
     document.getElementById('reader').style.display = 'none';
     document.getElementById('pokedex-content').style.display = 'flex';
     document.getElementById('main-text').innerHTML = data.text;
     document.getElementById('main-sprite').src = data.sprite;
-    pokemonDetectado = true;
-    setTimeout(() => { new Audio(data.cry).play().catch(() => {}); }, 300); 
-}
 
-function capturarPokemon() {
-    if (!pokemonDetectado) return;
-    const sprite = document.getElementById('main-sprite');
-    sprite.src = 'assets/img/pokeball.png'; // Tu imagen pixelada [cite: 2026-02-28]
-    sprite.classList.add('shaking-ball'); 
-    document.getElementById('main-text').innerHTML = "¡ATRÁPALO!";
     setTimeout(() => {
-        sprite.classList.remove('shaking-ball');
-        document.getElementById('main-text').innerHTML = "¡ATRÁPADO CON ÉXITO!";
-        pokemonDetectado = false;
-    }, 3000);
+        const audioGrito = new Audio(data.cry);
+        audioGrito.play().catch(e => console.log("Grito no encontrado"));
+    }, 300); 
 }
